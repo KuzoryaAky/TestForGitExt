e1e4d4525fdd8834c4879905d5d97451959dbea9
Yaroslav A <urfin1992@gmail.com>
2023-04-23 16:31:42 +0300

    fix

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello world!");
+Console.WriteLine("Приивет вы проебали");
+Console.WriteLine("Hello world!");
 Console.WriteLine("Komit 1");
 Console.WriteLine("Komit 2");
 Console.WriteLine("Komit 3");
